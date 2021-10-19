4f11cfbc833328037a4823ce8fcd3cc85b2669ab
Darina <37955869+dagenator@users.noreply.github.com>
2021-10-19 18:47:15 +0500

    Initial commit

---
+++ PointsCloudMovementLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PointsCloudMovementLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
