55c52c78bffe6dea136ff85d1f06befbe43e4859
Darina <37955869+dagenator@users.noreply.github.com>
2021-10-19 19:43:39 +0500

    Add classs point3D
    Add class Point3D on base of  MathNet.Numerics.LinearAlgebra.Vector;

--- PointsCloudMovementLibrary/CloudMoveLibrary.cs
+++ PointsCloudMovementLibrary/CloudMoveLibrary.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+MathNet.Numerics.LinearAlgebra.Vector;
 
 namespace PointsCloudMovementLibrary
 {
-    class Class1
+    class CloudMoveLibrary
     {
+        class Point3D
+        {
+            public Vector<double> V { private set; get; }
+
+            public Point3D(double x, double y, double z)
+            {
+                V[0] = x;
+                V[1] = y;
+                V[2] = z;
+            }
+
+
+        }
+
+        public Matrix<double> MovementMatrixCalcutation(Point3D x0, double x1, double y0, double y1, double z0, double z1)
+        {
+
+        }
+
+
+
+
     }
 }
